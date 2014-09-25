57951055faed75fd7fa68a17a32e571c7622a7f3
Mario Viapiano <marioeviapiano@gmail.com>
2014-09-25 12:24:29 -0300

    Add chatroom capabilities

--- app.js
+++ app.js
@@ -2,35 +2,25 @@ var server = require('http').createServer();
 var io = require('socket.io')(server);
 
 io.sockets.on('connection', function (socket) {
+  var joinedRoom = null;
+
   console.log('socket connected');
 
   socket.on('join', function (room) {
     console.log('Joined room ' + room);
     socket.join(room);
-  });
+    joinedRoom = room;
 
+    socket.broadcast.to(room).emit('new_user', { message: 'WELCOME ' + socket.id } );
+  });
 
   socket.on('disconnect', function () {
     console.log('socket disconnected');
   });
 
-  // io.sockets.socket(socket.id).
-  socket.broadcast.emit('new_user', { message: 'WELCOME ' + socket.id } );
-
-  // socket.on('my other event', function(data) {
-  //   console.log(data);
-
-  //   console.log(data.user_id);
-  //   console.log(data.message);
-  // });
-
-  // socket.on('send_message', function(data) {
-  //   console.log(data.message);
-  //   socket.broadcast.emit('get_message', data);
-  // });
 
   socket.on('send_message', function(data){
-    socket.broadcast.emit('get_message', data);
+    socket.broadcast.to(joinedRoom).emit('get_message', data);
   });
 
 });
